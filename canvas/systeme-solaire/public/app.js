66647fdc6b536b10f0d8c88c47a5644d6a1f5602
BillyRonico <billyronico412@gmail.com>
2021-05-14 08:24:52 +0400

    Correction systeme-solaire

--- canvas/systeme-solaire/public/app.js
+++ canvas/systeme-solaire/public/app.js
@@ -1,20 +1,28 @@
 "use strict";
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext("2d");
-let rayonOrbite = 100;
-let rayonSoleil = 25;
-let rayonTerre = 15;
-let rayonLune = 5;
+let ratio = innerWidth < innerHeight ? innerWidth : innerHeight;
+let rayonOrbiteTerre = ratio / 3;
+let rayonSoleil = ratio / 10;
+let rayonTerre = ratio / 20;
+let rayonLune = ratio / 40;
+let rayonOrbiteLune = ratio / 10;
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 window.addEventListener('resize', () => {
+    ratio = innerWidth < innerHeight ? innerWidth : innerHeight;
+    rayonOrbiteTerre = ratio / 3;
+    rayonSoleil = ratio / 10;
+    rayonTerre = ratio / 20;
+    rayonLune = ratio / 40;
+    rayonOrbiteLune = ratio / 10;
     canvas.width = innerWidth;
     canvas.height = innerHeight;
-    dessin();
 });
 function dessin() {
     // Fonction permettant de dessiner nos elements
     let date = new Date();
+    console.log(date.getSeconds());
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     ctx.translate(canvas.width / 2, canvas.height / 2);
@@ -26,23 +34,23 @@ function dessin() {
     ctx.closePath();
     // Dessin de l'orbite de la terre
     ctx.beginPath();
-    ctx.arc(0, 0, rayonOrbite, 0, 2 * Math.PI);
+    ctx.arc(0, 0, rayonOrbiteTerre, 0, 2 * Math.PI);
     ctx.lineWidth = 0.5;
     ctx.strokeStyle = "#FFFFFF";
     ctx.stroke();
     ctx.closePath();
     // Dessin de la terre
     ctx.beginPath();
-    ctx.rotate((2 * Math.PI / 60) * date.getSeconds() + (2 * Math.PI / 60000) * date.getMilliseconds());
-    ctx.arc(rayonOrbite, 0, rayonTerre, 0, 2 * Math.PI);
+    ctx.rotate((2 * Math.PI / 60) * date.getSeconds() + (2 * Math.PI / 60000) * date.getMilliseconds() - Math.PI * (1 / 2));
+    ctx.arc(rayonOrbiteTerre, 0, rayonTerre, 0, 2 * Math.PI);
     ctx.fillStyle = '#1010FF';
     ctx.fill();
     ctx.closePath();
     // Dessin de la lune
-    ctx.translate(rayonOrbite, 0);
+    ctx.translate(rayonOrbiteTerre, 0);
     ctx.beginPath();
-    ctx.rotate((2 * Math.PI / 6) * date.getSeconds() + (2 * Math.PI / 6000) * date.getMilliseconds());
-    ctx.arc(rayonSoleil, 0, rayonLune, 0, 2 * Math.PI);
+    ctx.rotate((2 * Math.PI / 6) * date.getSeconds() + (2 * Math.PI / 6000) * date.getMilliseconds() - Math.PI * (1 / 2));
+    ctx.arc(rayonOrbiteLune, 0, rayonLune, 0, 2 * Math.PI);
     ctx.fillStyle = "#888888";
     ctx.fill();
     ctx.closePath();
--- canvas/systeme-solaire/src/app.ts
+++ canvas/systeme-solaire/src/app.ts
@@ -1,29 +1,41 @@
 const canvas = document.getElementById('my-canvas') as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
-let rayonOrbite = 100
-let rayonSoleil = 25
-let rayonTerre = 15
-let rayonLune = 5
+let ratio = innerWidth < innerHeight ? innerWidth : innerHeight
+
+let rayonOrbiteTerre = ratio / 3
+let rayonSoleil = ratio / 10
+let rayonTerre = ratio / 20
+let rayonLune = ratio / 40
+let rayonOrbiteLune = ratio / 10
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
 window.addEventListener('resize', () => {
+
+    ratio = innerWidth < innerHeight ? innerWidth : innerHeight
+
+    rayonOrbiteTerre = ratio / 3
+    rayonSoleil = ratio / 10
+    rayonTerre = ratio / 20
+    rayonLune = ratio / 40
+    rayonOrbiteLune = ratio / 10
+
     canvas.width = innerWidth
     canvas.height = innerHeight
-    dessin()
 })
 
 function dessin() {
     // Fonction permettant de dessiner nos elements
 
     let date = new Date()
+    console.log(date.getSeconds())
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.save()
 
-    ctx.translate(canvas.width/2, canvas.height/2)
+    ctx.translate(canvas.width / 2, canvas.height / 2)
 
     // Dessin du soleil
     ctx.beginPath()
@@ -34,7 +46,7 @@ function dessin() {
 
     // Dessin de l'orbite de la terre
     ctx.beginPath()
-    ctx.arc(0, 0, rayonOrbite, 0, 2 * Math.PI)
+    ctx.arc(0, 0, rayonOrbiteTerre, 0, 2 * Math.PI)
     ctx.lineWidth = 0.5
     ctx.strokeStyle = "#FFFFFF"
     ctx.stroke()
@@ -42,17 +54,17 @@ function dessin() {
 
     // Dessin de la terre
     ctx.beginPath()
-    ctx.rotate((2 * Math.PI / 60) * date.getSeconds() + (2 * Math.PI / 60000) * date.getMilliseconds()) 
-    ctx.arc(rayonOrbite, 0, rayonTerre, 0, 2 * Math.PI)
+    ctx.rotate((2 * Math.PI / 60) * date.getSeconds() + (2 * Math.PI / 60000) * date.getMilliseconds() - Math.PI * (1 / 2))
+    ctx.arc(rayonOrbiteTerre, 0, rayonTerre, 0, 2 * Math.PI)
     ctx.fillStyle = '#1010FF'
     ctx.fill()
     ctx.closePath()
 
     // Dessin de la lune
-    ctx.translate(rayonOrbite, 0)
+    ctx.translate(rayonOrbiteTerre, 0)
     ctx.beginPath()
-    ctx.rotate((2 * Math.PI / 6) * date.getSeconds() + (2 * Math.PI / 6000) * date.getMilliseconds())
-    ctx.arc(rayonSoleil, 0, rayonLune, 0, 2 * Math.PI)
+    ctx.rotate((2 * Math.PI / 6) * date.getSeconds() + (2 * Math.PI / 6000) * date.getMilliseconds() - Math.PI * (1 / 2))
+    ctx.arc(rayonOrbiteLune, 0, rayonLune, 0, 2 * Math.PI)
     ctx.fillStyle = "#888888"
     ctx.fill()
     ctx.closePath()
@@ -60,7 +72,7 @@ function dessin() {
     ctx.restore()
 
     requestAnimationFrame(dessin)
-    
+
 }
 
 dessin()
