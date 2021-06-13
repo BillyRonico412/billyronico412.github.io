3dbea5d4141fcc6d3b9111292415ffe2ce875a0b
BillyRonico <billyronico412@gmail.com>
2021-06-13 16:56:28 +0400

    Ajout de Projet 3

---
+++ 30-jours-code/3-card-basics-2/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "color-carte-1": "hsl(31, 77%, 52%)",
+        "color-carte-2": "hsl(184, 100%, 22%)",
+        "color-carte-3": "hsl(179, 100%, 13%)"
+      }
+    },
+  },
+  variants: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
