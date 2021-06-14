9c678c84625cc36bd1a887592cee67094618f0a4
BillyRonico <billyronico412@gmail.com>
2021-06-14 14:34:46 +0400

    add projet 5 / 30

---
+++ 30-jours-code/5-social-proof/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "dark-magenta": "hsl(300, 43%, 22%)",
+        "light-magenta-bis": "#F7F2F8",
+        "soft-pink": "hsl(333, 80%, 67%)",
+        "dark-magenta-bis": "hsl(303, 10%, 53%)",
+        "light-magenta": "hsl(300, 24%, 96%)"
+      }
+    },
+  },
+  variants: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
