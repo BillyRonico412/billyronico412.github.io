5d92806799ec646f6f018f69b5e663f641c841bd
BillyRonico <billyronico412@gmail.com>
2021-06-15 20:05:05 +0400

    add project 9 / 30

---
+++ 30-jours-code/9-landing-page-1/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "violet": "hsl(257, 40%, 49%)",
+        "soft-magenta": "hsl(300, 69%, 71%)"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
